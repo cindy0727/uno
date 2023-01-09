4ceb660abca0aa1a9d34c5c958c74e8d13eb88ad
Janet1117 <janet911117@gmail.com>
2023-01-09 19:09:02 +0800

    謝卉絜:更新cardfunction.h

--- cardfunction.h
+++ cardfunction.h
@@ -9,19 +9,19 @@
 
 void SpecialCardFunction(node *player, int draw, int *PlayerOrder, int *OrderNumber, int playerAmount);
 void Reverse(int *player_order, int player_number);
-void DrawTwo(node *player);
-void DrawFour(node *player);
+void DrawTwo(int *playerOrder, int orderNumber, int playerAmount);
+void DrawFour(int *playerOrder, int orderNumber, int playerAmount);
 
 
-//執行功能牌功能
+//執行功能牌功能  格式(以玩家2為): SpecialCardFunction(player2, draw, ); 其他3個參數我來填
 void SpecialCardFunction(node *player, int draw, int *PlayerOrder, int *OrderNumber, int playerAmount){
     //+2
     if(draw == 2){
-        DrawTwo(player);
+        DrawTwo(PlayerOrder, *OrderNumber, playerAmount);
     }
     //+4
     else if(draw == 4){
-        DrawFour(player);
+        DrawFour(PlayerOrder, *OrderNumber, playerAmount);
     }
     //skip
     else if(draw == 5){
@@ -44,16 +44,77 @@ void Reverse(int *player_order, int player_number){
 }
 
 //+2
-void DrawTwo(node *player){
-    for(i = 0; i < 2; i++){
-        player = DrawOne(player);
+void DrawTwo(int *playerOrder, int orderNumber, int playerAmount){
+
+    if(playerAmount == 3){
+        if(playerOrder[orderNumber] == 0){
+            for(i = 0; i < 2; i++){
+                player2 = DrawOne(player2);
+            }
+        }else if(playerOrder[orderNumber] == 1){
+            for(i = 0; i < 2; i++){
+                player3 = DrawOne(player3);
+            }
+        }else if(playerOrder[orderNumber] == 2){
+            for(i = 0; i < 2; i++){
+                player1 = DrawOne(player1);
+            }
+        }
+    }else if(playerAmount == 4){
+        if(playerOrder[orderNumber] == 0){
+            for(i = 0; i < 2; i++){
+                player2 = DrawOne(player2);
+            }
+        }else if(playerOrder[orderNumber] == 1){
+            for(i = 0; i < 2; i++){
+                player3 = DrawOne(player3);
+            }
+        }else if(playerOrder[orderNumber] == 2){
+            for(i = 0; i < 2; i++){
+                player4 = DrawOne(player4);
+            }
+        }else if(playerOrder[orderNumber] == 3){
+            for(i = 0; i < 2; i++){
+                player1 = DrawOne(player1);
+            }
+        }
     }
 }
 
 //+4
-void DrawFour(node *player){
-    for(i = 0; i < 4; i++){
-        player = DrawOne(player);
+void DrawFour(int *playerOrder, int orderNumber, int playerAmount){
+    if(playerAmount == 3){
+        if(playerOrder[orderNumber] == 0){
+            for(i = 0; i < 4; i++){
+                player2 = DrawOne(player2);
+            }
+        }else if(playerOrder[orderNumber] == 1){
+            for(i = 0; i < 4; i++){
+                player3 = DrawOne(player3);
+            }
+        }else if(playerOrder[orderNumber] == 2){
+            for(i = 0; i < 4; i++){
+                player1 = DrawOne(player1);
+            }
+        }
+    }else if(playerAmount == 4){
+        if(playerOrder[orderNumber] == 0){
+            for(i = 0; i < 4; i++){
+                player2 = DrawOne(player2);
+            }
+        }else if(playerOrder[orderNumber] == 1){
+            for(i = 0; i < 4; i++){
+                player3 = DrawOne(player3);
+            }
+        }else if(playerOrder[orderNumber] == 2){
+            for(i = 0; i < 4; i++){
+                player4 = DrawOne(player4);
+            }
+        }else if(playerOrder[orderNumber] == 3){
+            for(i = 0; i < 4; i++){
+                player1 = DrawOne(player1);
+            }
+        }
     }
 }
 
