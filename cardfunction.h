c6bef81c1de0ac2da5ffe9d2b0cec5b7a7322779
Janet1117 <janet911117@gmail.com>
2023-01-10 03:05:29 +0800

    修正加牌

--- cardfunction.h
+++ cardfunction.h
@@ -8,8 +8,8 @@
 
 void SpecialCardFunction(int draw, int *PlayerOrder, int *OrderNumber, int playerAmount);
 node *DrawOne(node *player);//加一張牌
-void DrawTwo(int *playerOrder, int orderNumber, int playerAmount);
-void DrawFour(int *playerOrder, int orderNumber, int playerAmount);
+void DrawTwo(int *playerOrder, int *orderNumber, int playerAmount);
+void DrawFour(int *playerOrder, int *orderNumber, int playerAmount);
 
 int RevserseOrNot = 0;
 
@@ -18,11 +18,11 @@ int RevserseOrNot = 0;
 void SpecialCardFunction(int draw, int *PlayerOrder, int *OrderNumber, int playerAmount){
     //+2
     if(draw == 2){
-        DrawTwo(PlayerOrder, *OrderNumber, playerAmount);
+        DrawTwo(PlayerOrder, OrderNumber, playerAmount);
     }
     //+4
     else if(draw == 4){
-        DrawFour(PlayerOrder, *OrderNumber, playerAmount);
+        DrawFour(PlayerOrder, OrderNumber, playerAmount);
     }
     //skip
     else if(draw == 5){
@@ -60,14 +60,15 @@ node *DrawOne(node *player){
 }
 
 //+2
-void DrawTwo(int *playerOrder, int orderNumber, int playerAmount){
-    int next = orderNumber;
+void DrawTwo(int *playerOrder, int *orderNumber, int playerAmount){
+    int next = *orderNumber;
     if(RevserseOrNot % 2 == 0){
         next = (next == playerAmount - 1) ? 0 : (next + 1);
+        printf("r = 0, next = %d, order[next] = %d\n", next, playerOrder[next]);
     }else{
         next = (next == 0) ? (playerAmount - 1) : (next - 1);
+        printf("r = 1, next = %d, order[next] = %d\n", next, playerOrder[next]);
     }
-    next = (playerAmount - 1) ? 0 : next + 1;
     if(playerOrder[next] == 0){
         for(i = 0; i < 2; i++){
             player1 = DrawOne(player1);
@@ -88,12 +89,14 @@ void DrawTwo(int *playerOrder, int orderNumber, int playerAmount){
 }
 
 //+4
-void DrawFour(int *playerOrder, int orderNumber, int playerAmount){
-    int next = orderNumber;
+void DrawFour(int *playerOrder, int *orderNumber, int playerAmount){
+    int next = *orderNumber;
     if(RevserseOrNot % 2 == 0){
         next = (next == playerAmount - 1) ? 0 : (next + 1);
+        printf("r = 0, next = %d, order[next] = %d\n", next, playerOrder[next]);
     }else{
         next = (next == 0) ? (playerAmount - 1) : (next - 1);
+        printf("r = 1, next = %d, order[next] = %d\n", next, playerOrder[next]);
     }
     if(playerOrder[next] == 0){
         for(i = 0; i < 4; i++){
